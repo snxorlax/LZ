b8cfffe292ae317b41c86840958e9582eb26bc36
underthehoody <xxxlhoodie@gmail.com>
2024-09-04 21:47:32 -0400

    added rudimentary functionality for changing color schemes between pages. Homepage displays pink, about displays green. Will need to be refactored and animated, but good enough PoC to show Lily and begin to experiment with multiple schemes.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-const pages = ["home-page", "about-page", "lessons-page"]
+const pages = ["home-page", "about-page", "lessons-page"];
 
-function setActivePage(page){
-    let activePage  = document.getElementById(page);
-    activePage.style.display = "flex";
+function setActivePage(page) {
+    let activePage = document.getElementById(page);
+    setPageStyles(page);
+    activePage.style.display = "grid";
 
-    for (let i=0; i<pages.length; i++){
-        if (page != pages[i]){
+    for (let i = 0; i < pages.length; i++) {
+        if (page != pages[i]) {
             document.getElementById(pages[i]).style.display = "none";
         }
     }
+}
+const navText = document.getElementsByClassName("nav-text");
+const bg = document.getElementById("background");
+function setPageStyles(page) {
+    switch (page) {
+        case "home-page":
+            bg.style.backgroundColor = "white";
+            for (i = 0; i < navText.length; i++) {
+                navText[i].style.color = "#dc9ce2";
+            }
+            break;
+        case "about-page":
+            bg.style.backgroundColor = "#19DB8A";
+            for (i = 0; i < navText.length; i++) {
+                navText[i].style.color = "white";
+            }
+            break;
+        default:
+            break;
     }
-
+}
