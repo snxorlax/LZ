0fdc0823383a5b6d91391af2ecca5a63659711b7
underthehoody <xxxlhoodie@gmail.com>
2024-09-05 19:09:33 -0400

    narrowed down color choices, other simple fixes.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const pages = ["home-page", "about-page", "lessons-page"];
+const pages = ["home-page", "about-page", "lessons-page", "music-page"];
 
 function setActivePage(page) {
     let activePage = document.getElementById(page);
@@ -13,18 +13,22 @@ function setActivePage(page) {
 }
 const navText = document.getElementsByClassName("nav-text");
 const bg = document.getElementById("background");
+const logo = document.getElementById("logo");
 function setPageStyles(page) {
     switch (page) {
         case "home-page":
             bg.style.backgroundColor = "white";
             for (i = 0; i < navText.length; i++) {
                 navText[i].style.color = "#dc9ce2";
+                //navText[i].style.color = "#fda98f";
+                logo.style.color = "#dc9ce2";
             }
             break;
         case "about-page":
             bg.style.backgroundColor = "#19DB8A";
             for (i = 0; i < navText.length; i++) {
                 navText[i].style.color = "white";
+                logo.style.color = "white";
             }
             break;
         default:
